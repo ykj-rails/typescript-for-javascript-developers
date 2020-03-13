7866c9ce41d509afd5e0c86ceb33b05383172cd2
Yuki M <yukiji.0307@gmail.com>
2020-03-13 12:43:06 +0900

    dev merge

--- src/enum.ts
+++ src/enum.ts
@@ -1,3 +1,30 @@
-export {};
+export {}
 
-enum Months
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  Agust,
+  September,
+  October,
+  November,
+  December
+}
+
+console.log(Months.January)
+console.log(Months.February)
+console.log(Months.December)
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = 'FFFFFF',
+  GREEN = '008000',
+  BLUE = '#0000FF'
+}
+
+let green = COLORS.GREEN
+console.log({ green })
