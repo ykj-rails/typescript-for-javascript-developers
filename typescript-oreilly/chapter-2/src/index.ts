b832699f1a95231dda87c31765e16c4f7c66b639
松本雄揮 <yukiji.0307@gmail.com>
2020-10-12 17:24:48 +0900

    ts

---
+++ typescript-oreilly/chapter-2/src/index.ts
@@ -0,0 +1,21 @@
+let a = 1 + 2
+let b = a + 3
+let c = {
+  apple: a,
+  banana: b
+}
+
+let boolean = true
+
+let me: { name: string; age: number; kikon: boolean; tel: number } = {
+  name: 'ore',
+  age: 32,
+  kikon: false,
+  tel: 090
+}
+
+function xxx(b: number) {
+  return b / 2
+}
+
+a('z')
