d0a10ce97f2a5258b9af7f4032b3a0507757b5f6
松本雄揮 <yukiji.0307@gmail.com>
2020-10-17 10:04:37 +0900

    cmt

---
+++ typescript-oreilly/chapter-2/mondai.md
@@ -0,0 +1,19 @@
+## 3.4
+
+1.
+
+a. number
+b. string
+c. 'pineapples'
+d. boolean[]
+e. {type: string}
+f. (number | boolean)[]
+g. number[]
+h. any
+
+2.
+
+a. リテラル型の 3 に 4 を割り当てようとしているから
+b. number 型の配列に string 型を push しようとしているから
+c. never にはどの型も割り当てられない
+d. unknow 型に演算しようとしている
--- typescript-oreilly/chapter-2/src/index.ts
+++ typescript-oreilly/chapter-2/src/index.ts
@@ -14,8 +14,33 @@ let me: { name: string; age: number; kikon: boolean; tel: number } = {
   tel: 090
 }
 
+let me2: Person = {
+  name: 'ore',
+  age: 32,
+  kikon: false,
+  tel: 090,
+}
+
 function xxx(b: number) {
   return b / 2
 }
 
-a('z')
+// a('z')
+
+type Person = {
+  name: string; age: number; kikon: boolean; tel: number
+}
+
+let array: (number | string)[] = [1, 2, 3]
+
+array.push('3')
+
+let d = [true, true, false]
+const g = [3]
+let h = null
+
+type Add = (a: number, b: number) => number
+
+const add: Add = (a, b) => {
+  return a + b
+}
